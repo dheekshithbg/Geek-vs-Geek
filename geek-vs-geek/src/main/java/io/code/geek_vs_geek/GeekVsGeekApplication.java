3b8c96a9ca7de1be5bc0a670e76a7f259136d974
dheekshithbg <dheekshithbg@gmail.com>
2024-10-24 22:30:57 +0530

    Create Docker Image

--- geek-vs-geek/src/main/java/io/code/geek_vs_geek/GeekVsGeekApplication.java
+++ geek-vs-geek/src/main/java/io/code/geek_vs_geek/GeekVsGeekApplication.java
@@ -2,8 +2,10 @@ package io.code.geek_vs_geek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GeekVsGeekApplication {
 
 	public static void main(String[] args) {
